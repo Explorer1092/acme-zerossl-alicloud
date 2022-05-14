710e0fafaaa5f9ced4ebfffe7d0be11fcf1eb9c2
kevinliu.lk <kevinliu.lk@alibaba-inc.com>
2022-05-14 12:30:31 +0800

    fix output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "private_key" {
  value = acme_certificate.certificate.private_key_pem
 }
 
-output "private_key" {
- value = acme_certificate.certificate.private_key_pem
+output "certificate_pem" {
+ value = acme_certificate.certificate.certificate_pem
 }
